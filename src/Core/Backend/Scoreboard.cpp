5a762aea58acca21b8550b995269c6a91afb282f
YouqiXia <youqixiayi@gmail.com>
2024-01-02 12:38:46 +0800

    [feat]: add scoreboard

--- src/Core/Backend/Scoreboard.cpp
+++ src/Core/Backend/Scoreboard.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "Scoreboard.hpp"
+
+namespace TimingModel {
+
+    Scoreboard::Scoreboard(const std::string &name,
+                           TimingModel::PhyRegId_t phy_regfile_num,
+                           sparta::log::MessageSource &info_logger) :
+            score_board_(phy_regfile_num, 0),
+            phy_regfile_num_(phy_regfile_num)
+    {}
+
+    bool Scoreboard::IsForwarding(PhyRegId_t phy_reg_idx) {
+        sparta_assert(phy_reg_idx <= phy_regfile_num_, "Scoreboard access is out of range");
+        return score_board_[phy_reg_idx].isValid();
+    }
+
+    RobIdx_t Scoreboard::GetForwardingEntry(PhyRegId_t phy_reg_idx) {
+        sparta_assert(phy_reg_idx <= phy_regfile_num_, "Scoreboard access is out of range");
+        return score_board_[phy_reg_idx].getValue();
+    }
+
+    void Scoreboard::SetForwardingEntry(PhyRegId_t phy_reg_idx, RobIdx_t rob_idx) {
+        sparta_assert(phy_reg_idx <= phy_regfile_num_, "Scoreboard access is out of range");
+        score_board_[phy_reg_idx] = rob_idx;
+    }
+
+    void Scoreboard::ClearForwardingEntry(PhyRegId_t phy_reg_idx) {
+        sparta_assert(phy_reg_idx <= phy_regfile_num_, "Scoreboard access is out of range");
+        score_board_[phy_reg_idx].clearValid();
+    }
+}
--- src/Core/Backend/Scoreboard.hpp
+++ src/Core/Backend/Scoreboard.hpp
@@ -2,7 +2,40 @@
 // Created by yzhang on 1/1/24.
 //
 
-#ifndef MODEL_SCOREBOARD_HPP
-#define MODEL_SCOREBOARD_HPP
+#pragma once
 
-#endif //MODEL_SCOREBOARD_HPP
+#include "sparta/log/MessageSource.hpp"
+#include "sparta/utils/ValidValue.hpp"
+
+#include "sparta/utils/SpartaAssert.hpp"
+
+#include <string>
+
+#include "basic/Inst.hpp"
+#include "basic/InstGroup.hpp"
+
+namespace TimingModel {
+
+    using ScoreBoardEntry = sparta::utils::ValidValue<RobIdx_t>;
+
+    class Scoreboard {
+    public:
+
+        Scoreboard(const std::string &name,
+                   PhyRegId_t phy_regfile_num,
+                   sparta::log::MessageSource &info_logger);
+
+        bool IsForwarding(PhyRegId_t);
+
+        RobIdx_t GetForwardingEntry(PhyRegId_t);
+
+        void SetForwardingEntry(PhyRegId_t, RobIdx_t);
+
+        void ClearForwardingEntry(PhyRegId_t);
+
+    private:
+        std::vector<ScoreBoardEntry> score_board_;
+        PhyRegId_t phy_regfile_num_;
+    };
+
+}
