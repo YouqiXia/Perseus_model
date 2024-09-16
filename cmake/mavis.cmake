01ee89b5d212874649214f3cd858c6d1a0a5671f
YouqiXia <youqixiayi@gmail.com>
2024-09-16 18:23:04 +0800

    [fix]<done> fix mavis cmake compile options: -fPIC

--- cmake/mavis.cmake
+++ cmake/mavis.cmake
@@ -3,6 +3,7 @@
 set (MAVIS_BASE ${Perseus_BASE}/thirdparty/mavis)
 
 add_subdirectory(thirdparty/mavis)
+target_compile_options(mavis PRIVATE -fPIC)
 
 list(APPEND Perseus_INCLUDES ${MAVIS_BASE})
 list(APPEND Perseus_LIBS mavis)
